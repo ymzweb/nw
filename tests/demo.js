36fae1e5155a7688cbaf95f5e8bd96d4c0481648
y <y@126.com>
2017-08-15 23:20:43 +0800

    v2

---
+++ tests/demo.js
@@ -0,0 +1,24 @@
+// tests/demo.js
+module.exports = {
+ 
+  '01': function (client) {
+    client.url('https://www.baidu.com');
+  },
+  
+  '02': function(client){
+    // for (let i = 0; i < 5; i++) {
+    //     client.clearValue('#kw');
+        
+    //     client.setValue('#kw', i);
+        
+    //     client.click('#su');
+    // }
+   
+    
+  },
+
+   xx: function(client){
+        // client.end();
+        
+    }
+}
